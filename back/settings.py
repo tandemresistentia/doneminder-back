e081f3d684cb5ce0136731c217b41330f6b4a347
Luis <deathghoul41@gmail.com>
2023-08-21 23:52:18 +0200

    final

--- back/settings.py
+++ back/settings.py
@@ -19,9 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 AUTH_USER_MODEL = 'accounts.CustomUser'
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
 import os
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
 ENVIRONMENT = 'production'
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'production':
